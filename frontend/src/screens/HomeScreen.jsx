3fdc976f766085cf1bdbd623def1a49f2c46e7fe
AbdElMajidOuhamane <abdelmajidouhamane01@gmail.com>
2023-09-29 13:10:08 +0100

    add RTK state management

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,32 @@
-import React, { useEffect, useState } from 'react'
-// import products from '../products'
-import axios from 'axios'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../Components/Product'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+import Loader from '../Components/Loader'
+
+
 const HomeScreen = () => {
-  const [products,setProducts]=useState([])
-  useEffect(() => {
-    const fetchData=async()=>{
-      const {data}= await axios.get('/api/products');
-      setProducts(data)
-    }
-  fetchData()
-  }, [])
+  const {data:products,isLoading,error}=useGetProductsQuery()
 
   return (
     <div>
-        <h1>Latest Products</h1>
-        <Row>
-        {products.map((product)=>{
-            return(
-                <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                    <Product product={product}/>
-                </Col>
-            )
-        })}
-      </Row>
+      {isLoading ? (
+      <Loader />)
+      : error ?(
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+      <h1>Latest Products</h1>
+      <Row>
+      {products.map((product)=>{
+          return(
+              <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                  <Product product={product}/>
+              </Col>
+          )
+      })}
+    </Row>
+      </>)}
+        
     </div>
   )
 }
---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,23 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+
+export const productsApiSlice=apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+        getProducts:builder.query({
+            query:()=>({
+                url:PRODUCTS_URL,
+            }),
+            keepUnusedDataFor:3,
+        }),
+        getProductDetails:builder.query({
+            query:(productId)=>({
+                url:`${PRODUCTS_URL}/${productId}`,
+
+            }),
+            keepUnusedDataFor:3,
+        })
+    }),
+})
+
+export const {useGetProductsQuery,useGetProductDetailsQuery}=productsApiSlice;
